9a97d70c901de418afd156ddc69a06e4ca7de21e
Renowahode <wencedantedeveraddc@gmail.com>
2024-12-20 22:28:17 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App!</h1>
+      <p>This is a simple React application deployed using Vercel.</p>
+    </div>
+  );
+
+}
+
+export default App;
